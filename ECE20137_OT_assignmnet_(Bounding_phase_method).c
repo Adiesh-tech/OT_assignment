a6a2a9023e6791a85d68ddee734b429a2f07cc20
Adiesh-tech <85075259+Adiesh-tech@users.noreply.github.com>
2022-06-13 22:31:15 +0530

    Update ECE20137_OT_assignmnet_(Bounding_phase_method).c
    added some functions like pow(); on 13-06-2022

--- ECE20137_OT_assignmnet_(Bounding_phase_method).c
+++ ECE20137_OT_assignmnet_(Bounding_phase_method).c
@@ -1,21 +1,25 @@
-/* program to demonstrate the usage of the bounding phase method in C. */  
-#include <stdio.h>  
+/* program to demonstrate the usage of the bounding phase method in C. */
+#include <stdio.h>
 #include <math.h>
 #include <conio.h>
 float func(float a)
 {
-    float f = (a*a*a*a*2)-(a*a*a)+(5*a*a)-(12*a)+1;//the given function
-} 
-void main()
+    float function1 = 2*(pow(a,4)) - (pow(a,3)) + 5*(pow(a,2)) - 12*a + 1;// the given function
+    //float function2 = pow(a,2)+(54/a);
+    return function1;
+}
+int main()
 {
     float start, end, k = 0, delta, x, buff, pre;//declaration of variables
     printf (" \n Enter the initial guess point: ");  // inputing of the initial value
     scanf (" %f", &x);
     printf (" \n Enter the increment value: ");  // inputing of the increment value
     scanf (" %f", &delta);
+    //x = 0;
+    //delta = 1;
     start = x - delta ; // the point towards the left of the initial point
     end = x + delta ; // the point towards the right of the initial point
-    
+
     if(func(start)>=func(x)&&func(x)<=func(end))
     {
         printf(" \n The Minimum of the function lies in between %f and %f",start, end);
@@ -30,8 +34,8 @@ void main()
         {
             delta = -delta ;
         }
-    
-        buff = x + pow(2,k)*delta ; //itrerative formula to find the next point
+
+        buff = x + pow(2,k)*delta ; //iterative formula to find the next point
         while(func(buff)<func(x))
         {
             pre = x ;
@@ -40,5 +44,7 @@ void main()
             buff = x + pow(2,k)*delta ;
         }
         printf(" \n The Minimum of the function lies in between %f and %f",pre,buff);
+        printf(" \n The number of iterations are %f",k);
     }
+    return 0;
 }
